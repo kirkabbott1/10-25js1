d7b58f66e49d753daff05b285da4ed8128c8da86
Kirk Abbott <kirkabbott1@gmail.com>
2016-10-27 09:51:58 -0400

    adds some function practice

--- functions.js
+++ functions.js
@@ -1,46 +1,16 @@
-// function hello(name) {
-//   console.log('hello, ' + name);
-// hello('kirk');
 
 
-// function hello(name) {
-//   if (!name) {
-//     name = 'world';
-//   }
-//   console.log('hello, ' + name);
-//   }
-// hello();
+function factor2(num) {
+  var factors = [];
+  for (var i = 1; i < Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      factors.push(i);
 
-// function madlib(name, subject) {
-//   console.log( name + "'s favorite subject in school is " + subject;
-// }
-//
-// madlib('kirk', 'js');
+      if (i !== num) {
+      factors.push(num / i);
+      }
 
-function tipAmount(bill, quality) {
-  if (quality === 'fair') {
-    console.log(bill * .15)
-    return bill * .15;
-
-  }
-  else if (quality === 'good') {
-    return bill * .2;
-  }
-  else if (quality === 'Bad') {
-    return bill * .1;
-  }
+    }
+  return factors;
 }
- tipAmount(14563, 'Bad');
-
-
-function totalAmount(bill, quality) {
-  var total = bill + tipAmount(bill, quality);
-  return total;
-  }
-totalAmount(14563, 'Bad');
-
-function splitAmount(bill, quality, ppl) {
-  var split = totalAmount(bill, quality)  / ppl;
-  console.log(split);
-  }
-splitAmount(100, 'good', 5);
+console.log(factor2(16));
--- rockpaper.js
+++ rockpaper.js
@@ -11,6 +11,9 @@ function rockPaperScissors(throw1, throw2) {
   else if (throw1 == "scissors" && throw2 == "rock") {
     return "player 2 won with rock over scissors"
   }
+  else if (throw1 === "scissors" && throw2 === "paper") {
+    return "player 1 wins with scissors over paper"
+  }
   else if (throw1 == "paper" && throw2 == "scissors") {
     return "player 2 won with scissors"
   }
@@ -18,4 +21,40 @@ function rockPaperScissors(throw1, throw2) {
     return "player 1 paper over rock"
   }
 }
+
+
+beats('rock', 'scissors')
+beats('rock', 'paper')
+beats('rock', 'rock')
+
 console.log(rockPaperScissors("paper", "rock"));
+
+function rockPaper2(throw1, throw2) {
+   if (throw1 == throw2) {
+       return 'draw';
+   }
+   if (throw1 == 'rock') {
+        if (throw2 == 'scissors') {
+           return 'player1';
+       } else {
+           return 'player2';
+       }
+   } else if (throw2 == 'rock') {
+       if (throw1 == 'scissors') {
+           return 'player2';
+       } else {
+           return 'player1';
+       }
+   } else if (throw1 == 'scissors') {
+       return 'player1';
+   } else {
+       return 'player2';
+   }
+}
+
+function positiveNumbers(arr) {
+  for (var i = 0; i < arr.length; i++) {
+
+  }
+
+}
